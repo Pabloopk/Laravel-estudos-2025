8b75f5a146c78da99807c4d3551ae284497cc43c
Pablo <parkour.pablo458@gmail.com>
2025-01-20 22:48:17 -0300

    add routes

--- App-FullStack/laravel_react/src/App.jsx
+++ App-FullStack/laravel_react/src/App.jsx
@@ -1,13 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 export default function App() {
-
-
-  return (
-   <>
-    Hello
-   </>
-  );
+  return<BrowserRouter>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+  </BrowserRouter>;
 }
 
 
