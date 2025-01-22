b207dc721bc59a1b2bcc2e929b1690e540019482
Pablo <parkour.pablo458@gmail.com>
2025-01-21 22:58:36 -0300

    adding login and register to the layout.

---
+++ App-FullStack/laravel_react/src/Pages/Auth/Login.jsx
@@ -0,0 +1,7 @@
+export default function Login(){
+    return(
+        <>
+            <h1 className="title">Faça seu Login</h1>
+        </>
+    );
+}
--- App-FullStack/laravel_react/src/Pages/Layout.jsx
+++ App-FullStack/laravel_react/src/Pages/Layout.jsx
@@ -7,6 +7,11 @@ export default function Layout(){
             <header>
                 <nav>
                     <Link to="/" className="nav-link">Home</Link>
+
+                    <div className="space-x-4">
+                        <Link to="/register" className="nav-link">Registre-se</Link>
+                        <Link to="/login" className="nav-link">Entrar</Link>
+                    </div>
                 </nav>
             </header>
             <main>
