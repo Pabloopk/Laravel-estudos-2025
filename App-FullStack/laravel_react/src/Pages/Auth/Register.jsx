93bcff3bfc64c90b2964c63d9a285441233fc30c
Pablo <parkour.pablo458@gmail.com>
2025-01-21 23:30:06 -0300

    adding form to the register

--- App-FullStack/laravel_react/src/Pages/Auth/Register.jsx
+++ App-FullStack/laravel_react/src/Pages/Auth/Register.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+
 export default function Register(){
+
+    const [formData, setFormData] = useState({
+        name:  '',
+        email: '',
+        password:'',
+        password_confirmation: '',
+    })
+
+    function handleRegister(e){
+        e.preventDefault()
+        
+        console.log(formData);
+    }
+
     return(
         <>
             <h1 className="title">Registre Sua Nova Conta</h1>
+            
+            <form onSubmit={handleRegister} className="w-1/2 m-auto space-y-6">
+                <div>
+                    <input type="text" placeholder="Nome" value={formData.name}
+                        onChange={(e) => setFormData({...formData, name: e.target.value})}       
+                     />
+                </div>
+                <div>
+                    <input type="text" placeholder="Email" />
+                </div>
+                <div>
+                    <input type="password" placeholder="Senha" />
+                </div>
+                <div>
+                    <input type="password" placeholder="Confirme a Senha" />
+                </div>
+
+               <div className="primary-btn">
+                Registre-se
+               </div>
+                
+            </form>
         </>
     );
 }
