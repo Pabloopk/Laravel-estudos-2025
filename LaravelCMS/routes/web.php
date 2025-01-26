f4d764bd60ef4a1663fd5293ab93691dc7dbe768
Pablo <parkour.pablo458@gmail.com>
2025-01-25 23:56:19 -0300

    solution of my error

--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', action: 'Site\HomeController@index');
 
+Route::get('/', 'App\Http\Controllers\Site\HomeController@index');
 
-Route::prefix('painel')->group(function () {
-    Route::get('/', 'Admin\HomeController@index');
+Route::prefix('painel')-> group(function()
+{
+    Route::get('/', 'App\Http\Controllers\Admin\HomeController@index');
 });
 
