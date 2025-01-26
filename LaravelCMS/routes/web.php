115d5355ce41c99837d3260d05301c1293b4487f
Pablo <parkour.pablo458@gmail.com>
2025-01-25 23:51:46 -0300

    edit

--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'Site\HomeController@index');
+Route::get('/', action: 'Site\HomeController@index');
 
 
 Route::prefix('painel')->group(function () {
