74df35316549ef3943a5c33c2695b7c6ea6630de
Pablo <parkour.pablo458@gmail.com>
2025-01-25 23:36:21 -0300

    web

--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'Site\HomeController@index');
+
+
+Route::prefix('painel')->group(function () {
+    Route::get('/', 'Admin\HomeController@index');
 });
+
