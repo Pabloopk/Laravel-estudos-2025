c8291ae95ef0deef4d8c0a6f39f909cd01f012fc
Pablo <parkour.pablo458@gmail.com>
2025-02-18 10:34:45 -0300

    add redirect auth

---
+++ LaravelCMS/app/Http/Middleware/RedirectIfAuthenticate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $guard = null; // or specify the guard you want to use
+        if (Auth::guard($guard)->check()) {
+            return redirect('/home');
+        }
+        return $next($request);
+    }
+}
