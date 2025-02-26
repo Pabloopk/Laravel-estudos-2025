d9f005b65a8dda6aa763e787bf468b251bda8b77
Pablo <parkour.pablo458@gmail.com>
2025-02-26 19:59:13 -0300

    add

--- LaravelCMS/app/Http/Controllers/Admin/HomeController.php
+++ LaravelCMS/app/Http/Controllers/Admin/HomeController.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Auth;
+namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     //
     public function __construct() {
-        // $this->middleware('auth');
+        $this->middleware('auth');
     }
     public function index() {
         return view('admin.home');
---
+++ LaravelCMS/resources/views/admin/login.blade.php
@@ -0,0 +1,1 @@
+@section('adminlte::login')
