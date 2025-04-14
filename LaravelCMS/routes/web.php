9a12b356ec473bc9ef90b6bbfba9160109a6f0f0
Pablo <parkour.pablo458@gmail.com>
2025-04-14 15:13:45 -0300

    add

--- LaravelCMS/routes/web.php
+++ LaravelCMS/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\Site\HomeController@index');
 
-Route::prefix('painel')-> group(function()
+Route::prefix('painel')->group(function()
 {
     Route::get('/', 'App\Http\Controllers\Admin\HomeController@index')->name('admin');
     Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
