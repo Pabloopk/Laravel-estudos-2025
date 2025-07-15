abe505ad0cdfc40e8e3c90ad0cd60a3e341c61c5
Pablo <parkour.pablo458@gmail.com>
2025-07-14 21:43:04 -0300

    add ajustes de posts

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    const UPDATED_AT = 'updated_at';
+    const CREATED_AT = 'created_at';
+
+    protected $fillable = [
+        'title',
+        'content',
+        'cover',
+        'status',
+    ];
 }
