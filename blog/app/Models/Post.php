9d427f9e736ce560287b8013438bd17dd195f131
Pablo <parkour.pablo458@gmail.com>
2025-07-14 21:56:05 -0300

    add

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -4,16 +4,15 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     const UPDATED_AT = 'updated_at';
     const CREATED_AT = 'created_at';
 
-    protected $fillable = [
-        'title',
-        'content',
-        'cover',
-        'status',
-    ];
+  public function author(): BelongsTo
+  {
+      return $this->belongsTo(User::class, 'authorId');
+  }
 }
