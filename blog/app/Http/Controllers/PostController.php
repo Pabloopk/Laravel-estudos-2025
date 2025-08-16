7677c841d585e1d48f47e3b5b6e98d83b7163115
Pablo <parkour.pablo458@gmail.com>
2025-08-16 10:05:59 -0300

    codificando o gets posts no PostController

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -13,6 +13,16 @@ class PostController extends Controller
         // Logic to retrieve posts
         // For example, you might fetch posts from a Post model
         $posts = \App\Models\Post::all();
+        $returnData = [];
+        foreach ($posts as $post) {
+            $returnData[] = [
+                'id' => $post->id,
+                'title' => $post->title,
+                'cover' => $post->cover,
+                'createdAt' => $post->created_at,
+                'authorName' => $post->author->name,
+            ];
+        }
 
         return response()->json($posts);
     }
